5fe7d0081113409d679adcfe542fac5c739a2591
Aldo Pedraza <aldpdz@gmail.com>
2020-06-18 23:53:35 -0500

    Unit tests

--- app/src/test/java/com/example/productscanner/viewmodel/CameraViewModelTest.kt
+++ app/src/test/java/com/example/productscanner/viewmodel/CameraViewModelTest.kt
@@ -1,7 +1,35 @@
 package com.example.productscanner.viewmodel
 
+import android.view.View
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import com.example.productscanner.getOrAwaitValue
+import org.hamcrest.CoreMatchers.`is`
 
+@RunWith(AndroidJUnit4::class)
 class CameraViewModelTest{
+
+    // Subject under test
+    private lateinit var cameraViewModel: CameraViewModel
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setupViewModel(){
+        cameraViewModel = CameraViewModel()
+    }
+
+    @Test
+    fun atStart_buttonsAreVisible(){
+        // Then the buttons are visible
+        val value = cameraViewModel.btnVisibility.getOrAwaitValue()
+        assertThat(value, `is`(View.VISIBLE))
+    }
 }
