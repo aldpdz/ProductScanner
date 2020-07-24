31e66463f5abf67dc89372284fe7f335951e970e
Aldo Pedraza <aldpdz@gmail.com>
2020-07-24 15:44:34 -0500

    Update integration test

--- app/src/androidTest/java/com/example/productscanner/view/CameraFragmentTest.kt
+++ app/src/androidTest/java/com/example/productscanner/view/CameraFragmentTest.kt
@@ -10,12 +10,9 @@ import androidx.test.filters.MediumTest
 import com.example.productscanner.R
 import com.example.productscanner.di.ProductsRepositoryModule
 import com.example.productscanner.launchFragmentInHiltContainer
-import com.example.productscanner.data.network.FakeAndroidTestRepository
+import com.example.productscanner.repositories.FakeTestRepository
 import com.example.productscanner.repositories.IProductsRepository
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
@@ -23,8 +20,6 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import javax.inject.Inject
-import javax.inject.Singleton
 
 @MediumTest
 @RunWith(AndroidJUnit4::class)
@@ -32,11 +27,12 @@ import javax.inject.Singleton
 @HiltAndroidTest
 class CameraFragmentTest{
 
-    @Inject
-    lateinit var repository: IProductsRepository
-
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
+
+    @BindValue
+    @JvmField// avoid issues with Hilt
+    val repository: IProductsRepository = FakeTestRepository()
 
     @Before
     fun initRepository(){
@@ -51,14 +47,5 @@ class CameraFragmentTest{
         // THEN - the buttons must be visible
         onView(withId(R.id.btn_scan_sku)).check(matches(isDisplayed()))
         onView(withId(R.id.btn_scan_upc)).check(matches(isDisplayed()))
-    }
-
-    // Just for this class
-    @Module
-    @InstallIn(ApplicationComponent::class)
-    abstract class ProductsRepositoryTestModule{
-        @Singleton
-        @Binds
-        abstract fun bindProductsRepository(productsRepository: FakeAndroidTestRepository): IProductsRepository
     }
 }
--- app/src/androidTest/java/com/example/productscanner/view/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/productscanner/view/MainActivityTest.kt
@@ -10,42 +10,40 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.example.productscanner.R
+import com.example.productscanner.clearSharedPrefs
 import com.example.productscanner.di.ProductsRepositoryModule
-import com.example.productscanner.data.network.FakeAndroidTestRepository
 import com.example.productscanner.data.network.NetworkProduct
+import com.example.productscanner.repositories.FakeTestRepository
 import com.example.productscanner.repositories.IProductsRepository
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import javax.inject.Inject
-import javax.inject.Singleton
 
-@UninstallModules(ProductsRepositoryModule::class) // Ignore production module
 @LargeTest // End-to-end test
 @RunWith(AndroidJUnit4::class)
+@ExperimentalCoroutinesApi
+@UninstallModules(ProductsRepositoryModule::class) // Ignore production module
 @HiltAndroidTest
 class MainActivityTest{
-
-    @Inject
-    lateinit var repository: IProductsRepository
-
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
-    // TODO handle erase the share preference as with sql
+    @BindValue
+    @JvmField
+    val repository: IProductsRepository = FakeTestRepository()
+
     @Before
     fun initRepository(){
         hiltRule.inject()
     }
-
+    // TODO - Add idle
     @Test
     fun editPriceQuantity(){
         val product1 = NetworkProduct(
@@ -56,21 +54,24 @@ class MainActivityTest{
             "sku-product1",
             "upc-product1",
             1,
-            1.0f,
-            false
-        )
+            1.0f)
 
         // Set initial state
         // The initial state must be set before calling launch
-        repository.addProducts(product1)
+        runBlocking {
+            repository.saveProducts(listOf(product1))
+        }
 
         // Todo use better approach with resources
-//        getApplicationContext<Context>().getString(R.string.price)
         val prefixPrice = "Price: $"
         val prefixQuantity = "Quantity: "
 
         // Start up Products screen.
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        activityScenario.onActivity { activity ->
+            // Clear preferences
+            clearSharedPrefs(activity)
+        }
 
         // Click on the product on the list and verify that all the data is correct.
         onView(withText("Product1")).perform(click())
@@ -91,14 +92,5 @@ class MainActivityTest{
 
         // Important when you're working with a database
         activityScenario.close()
-    }
-
-    // Just for this class
-    @Module
-    @InstallIn(ApplicationComponent::class)
-    abstract class ProductsRepositoryTestModule{
-        @Singleton
-        @Binds
-        abstract fun bindProductsRepository(productsRepository: FakeAndroidTestRepository): IProductsRepository
     }
 }
